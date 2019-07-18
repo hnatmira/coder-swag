f33f40bb238475cf5c9a1c25ede6765251c60aae
Miroslav Hnat <miroslav.hnat@adastragrp.com>
2019-07-18 14:57:57 +0200

    initial commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Miroslav Hnát on 18/07/2019.
+//  Copyright © 2019 Miroslav Hnát. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
